c2854f0e58b380a442376c123196db16578d5cda
Ale Roses <ale.vrs@outlook.com>
2024-09-15 18:23:19 -0500

    Updates

--- utils/countdownDate.mjs
+++ utils/countdownDate.mjs
@@ -1,7 +1,7 @@
 // 1. Countdown target date
 const countdownDate = new Date(
   "Dec 31, 2024 23:59:59"
-  /* "Sep 9, 2024 20:28:00" */
+  /* "Sep 15, 2024 18:21:00" */
 ).getTime();
 
 export { countdownDate };
